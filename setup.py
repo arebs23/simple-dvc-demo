821fa91a3eb5f1d9ccff21ff5b4ecf60f8a6969e
Victor Oloyede Aregbede <aregbede60@gmail.com>
2021-03-25 22:37:39 +0100

    setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'src',
+    version = '0.0.1',
+    description = "it's a wine quality package",
+    author = 'Aregbede Victor',
+    packages = find_packages(),
+    license = 'MIT'
+)
